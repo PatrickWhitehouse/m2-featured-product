30bda3a76af977d12e99c6744a69fb431525366d
Patrick Whitehouse <patrick1996@gmail.com>
2022-01-18 12:56:27 +0000

    add store currency symbol

--- app/code/FoundationCommerce/ProductBlock/ViewModel/Product.php
+++ app/code/FoundationCommerce/ProductBlock/ViewModel/Product.php
@@ -8,20 +8,28 @@ use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Directory\Model\CurrencyFactory;
 
 class Product implements ArgumentInterface 
 {
 
-        protected $productRepository; 
-        protected $scopeConfig;
+        private $productRepository; 
+        private $scopeConfig;
+        private $scopeConfigInterface;
+        private $currencyCode;
         
         public function __construct(
                 ProductRepositoryInterface $productRepository,
-                ScopeConfigInterface $scopeConfig
+                ScopeConfigInterface $scopeConfig,
+                StoreManagerInterface $scopeConfigInterface,
+                CurrencyFactory $currencyFactory
                 )
         { 
                $this->productRepository = $productRepository;
                $this->scopeConfig = $scopeConfig;
+               $this->scopeConfigInterface = $scopeConfigInterface;
+               $this->currencyCode = $currencyFactory->create();
         }
 
         private function getSkuFromAdmin() {
@@ -40,6 +48,13 @@ class Product implements ArgumentInterface
                          return $this->productRepository->get($this->getSkuFromAdmin());
                 }
         }
+
+        public function getCurrencySymbol()
+    {
+        $currentCurrency = $this->scopeConfigInterface->getStore()->getCurrentCurrencyCode();
+        $currency = $this->currencyCode->load($currentCurrency);
+        return $currency->getCurrencySymbol();
+    }
 
 }
 
